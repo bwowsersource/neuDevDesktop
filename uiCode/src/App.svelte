595dac3fa67c69ba302970c44d8d38105f730570
Akash <akash@bwowser.com>
2023-01-20 02:56:25 -0500

    init

---
+++ uiCode/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+  // @ts-ignore
+ window.Neutralino.init(); // Add this function call
+
+ async function openVSCode(){
+  await Neutralino.os.execCommand('code /', { background: true });
+ }
+</script>
+
+<main>
+  <div>
+    <button on:click={openVSCode}>open VScode at root</button>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ uiCode/src/main.js
@@ -0,0 +1,31 @@
+import './app.css'
+// @ts-ignore
+import App from './App.svelte';
+
+import authInfo from '../../.tmp/auth_info.json'
+
+if (import.meta.env.DEV) {
+  const { accessToken, port } = authInfo
+  // @ts-ignore
+  window.NL_PORT = port
+  // @ts-ignore
+  window.NL_TOKEN = accessToken
+  // @ts-ignore
+  window.NL_ARGS = [
+    'bin\\neutralino-win_x64.exe',
+    '',
+    '--load-dir-res',
+    '--path=.',
+    '--export-auth-info',
+    '--neu-dev-extension',
+    '--neu-dev-auto-reload',
+    '--window-enable-inspector'
+  ]
+}
+
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
